fb52f058ab9ad46b7abba7daac638b7c2b30b6e0
ahaurw01 <aaron.haurwitz@gmail.com>
2020-05-13 08:42:19 -0400

    wrap board in a cutout

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types'
 import styles from './Layout.module.css'
+import { Cutout, Window, WindowContent } from 'react95'
 
 const Layout = ({ boardSlot, actionSlot, itemSlot }) => (
   <div className={styles.layout}>
     <div className={styles.actionBoardWrapper}>
       <div className={styles.action}>{actionSlot}</div>
-      <div className={styles.board}>{boardSlot}</div>
+      <div className={styles.board}>
+        <Window>
+          <WindowContent>
+            <Cutout
+              shadow={false}
+              style={{ width: '100%', height: 'calc(100vh - 240px)' }}
+            >
+              {boardSlot}
+            </Cutout>
+          </WindowContent>
+        </Window>
+      </div>
     </div>
     <div className={styles.item}>{itemSlot}</div>
   </div>
